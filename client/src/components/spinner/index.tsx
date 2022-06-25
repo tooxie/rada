c1ada4fb2e93bb2911ad8f78e3496789f635329d
Alvaro Mouriño <alvaro@mourino.net>
2022-06-25 10:13:48 +0200

    Make the Spinner's "Loading..." message variable

--- client/src/components/spinner/index.tsx
+++ client/src/components/spinner/index.tsx
@@ -3,10 +3,14 @@ import { h } from "preact";
 import style from "./style.css";
 import spinner from "./spinner.gif";
 
-const Spinner = () => (
+interface SpinnerProps {
+  message?: string;
+}
+
+const Spinner = ({ message }: SpinnerProps) => (
   <div class={style.spinner}>
-    <img src={spinner} />
-    <p>Loading...</p>
+    <img src={spinner} alt={message || "Loading..."} />
+    <p>{message || "Loading..."}</p>
   </div>
 );
 
