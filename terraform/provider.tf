52e03aeebfc990f02eae504d6260ecae0f6726f3
Alvaro Mouriño <alvaro.mourino@sennder.com>
2023-05-29 18:59:36 +0200

    Move locals to `locals.tf` file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -26,22 +26,3 @@ terraform {
     key = "terraform.tfstate"
   }
 }
-
-resource "random_uuid" "server_id" { }
-resource "random_string" "suffix" {
-  length = 6
-  special = false
-  upper = false
-}
-
-locals {
-  suffix = var.suffix != "" ? var.suffix : random_string.suffix.result
-  mime_types = jsondecode(file("${path.module}/mime.json"))
-  app_public_url_parameter_name = "GawshiAppPublicUrl-${local.suffix}"
-  cognito_admin_group_name = "Gawshi-Admin-${local.suffix}"
-  server_id = "server:${random_uuid.server_id.result}"
-}
-
-output "server_id" {
-  value = local.server_id
-}
