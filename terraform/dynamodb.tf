412ad890071e29346b88c7a22f2ef5df2d9fb36f
Alvaro Mouriño <alvaro@mourino.net>
2024-04-29 23:20:21 +0200

    Revert "Remove `timestamp` range key from invites table"
    This reverts commit c0b14f640410c7a7eb3d94d2577e717ba278ee07.

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -65,10 +65,16 @@ resource "aws_dynamodb_table" "invitations" {
   name = "GawshiInvitations-${local.suffix}"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "id"
+  range_key = "timestamp"
 
   attribute {
     name = "id"
     type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "N"
   }
 }
 
